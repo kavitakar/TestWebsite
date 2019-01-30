541150fbc4d25cd0f737376fc7d5a9e3cd3240d2
kavitakar <kavita.kar.ads@gmail.com>
2019-01-29 22:09:13 -0500

    Test Form- to be removed
    Test Form- demo only index is replaced with test form

--- wp-content/index.php
+++ wp-content/index.php
@@ -1,2 +1,37 @@
-<?php
-// Silence is golden.
+<?php 
+
+if(isset($_POST['submit'])) {
+    
+
+    $minimun = 5;
+    $maximun = 10;
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+    
+  
+    
+    
+    echo "Welcome";
+    
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Welcome to our food pantry! What would you like?</h2>
+
+    <h3> Pick Carbs </h3>
+<select name="Carbs">
+  <option value="Bread">Bread</option> 
+  <option value="Pasta (uncooked)">SPasta (uncooked)</option>
+  <option value="Noodles">Noodles</option>
+  <option value="Hamburger Buns">Hamburger Buns</option>
+</select>
+<br>
+<br>
+<button type="button" onclick="alert('Hello World!')">Click Me!</button>
+
+</body>
+</html>
